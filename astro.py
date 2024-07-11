c4f0c7a3ed9b69ee66953c121b95b4b5a1794fdb
SharnuSharath123 <sharnuburli143@gmail.com>
2024-07-11 20:39:04 +0530

    astro

---
+++ astro.py
@@ -0,0 +1,6 @@
+First_number = int(input("Enter the 1st number>> "))
+Second_number = int(input("Enter the 2nd number>> "))
+total = First_number + Second_number
+print(total)
+print("Hello World!!!")
+
